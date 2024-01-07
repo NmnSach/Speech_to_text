c388591d078a190aa20f8f42cbdfaebb1a7c3abf
Nmn <nssachdeva2@gmail.com>
2024-01-07 20:50:03 +0530

    api.js error fix

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const { Configuration, OpenAIApi } = require("openai");
+const { default: Configuration, OpenAIApi } = require("openai");
 
 const router = express.Router();
 const upload = multer();
